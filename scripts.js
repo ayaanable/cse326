925c4b22c01e7634e7b512032e95b9008a6bfce8
Ayaan Singh <ayaankumar2024@lpu.in>
2024-11-05 23:47:44 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
+function showMessage() {
+    alert('You are already at Home Page');}
